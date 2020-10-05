d57624ba7a570c29402a16916f593357c2005200
Buzzlay <dimabuslaev@gmail.com>
2020-10-05 19:50:29 +0500

    I tried to correct all the comments, but did not quite understand about the directive include

--- posts/admin.py
+++ posts/admin.py
@@ -8,4 +8,15 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
+
 admin.site.register(Post, PostAdmin)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "slug",)
+    search_fields = ("title",)
+    list_filter = ("slug",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Group, GroupAdmin)
